f84cbf93e3f04bc7146e0f653eacac2e1031949f
Bujnoch, Natalie <Natalie.Bujnoch@andersencorp.com>
2025-07-31 19:25:50 -0500

    Initial Commit

---
+++ SentimentVibe/SentimentVibeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SentimentVibeApp.swift
+//  SentimentVibe
+//
+//  Created by m_959058 on 7/31/25.
+//
+
+import SwiftUI
+
+@main
+struct SentimentVibeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
